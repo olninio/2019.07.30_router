6963fa8314bb71e20fa75b43fab57be3b493f493
Jakub Sawczuk <j.j.sawczuk@gmail.com>
2019-07-30 15:04:24 +0200

    setState :O

--- src/Contact.js
+++ src/Contact.js
@@ -3,24 +3,36 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 
-const Contact = (props) => (
-  <div>
-    <TextField
-        label="Message"
-        fullWidth = {true}
-    />
-    <br/>
-    <br/>
-    <Button
-      variant = {'contained'}
-      color = {'secondary'}
-      size = {'small'}
-      fullWidth = {true}
-      >
-      Contact Us
-    </Button>
-  </div>
-  
-)
+class Contact extends React.Component {
 
+  state = {
+    message: ''
+  }
+
+  render() {
+    return (
+      <div>
+        <TextField
+            label="Message"
+            fullWidth = {true}
+            value = {this.state.message}
+            onChange = {(event) => this.setState({
+              message: event.target.value
+            })}
+        />
+        <br/>
+        <br/>
+        <Button
+          variant = {'contained'}
+          color = {'secondary'}
+          size = {'small'}
+          fullWidth = {true}
+          onClick = {(event) => alert('click')} 
+          >
+          Contact Us
+        </Button>
+      </div> 
+    )
+  }
+}
 export default Contact;
